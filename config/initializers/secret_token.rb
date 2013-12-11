59d091c579d2c130ad8db86d32cd6e3935276475
Alexandre NARBONNE <alexandre.narbonne@aliceadsl.fr>
2013-12-11 21:00:16 +0100

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LooklaDev::Application.config.secret_key_base = '741424e5dd604ffb6849ac2220e1ba0bb4d3eb3a7ad0c81d9e0566467c4ad10dd039f858ad6abae03cb79cd90b286291e925dd1b9adb28f3adc95a2ddefe1245'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LooklaDev::Application.config.session_store :cookie_store, key: '_lookla-dev_session'
