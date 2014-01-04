4e79710e4ca11106db28ad983a3bccbb26e1fcf4
Blaked <dorian.becker@gadz.org>
2014-01-04 01:40:23 +0100

    add commande to personne view

--- app/controllers/personne_controller.rb
+++ app/controllers/personne_controller.rb
@@ -8,6 +8,8 @@ class PersonneController < ApplicationController
   def show
   	@personne = Personne.find(params[:id])
     @taillevetement = Taillevetement.find(@personne.taillevetement_id).name
+    @commande = Commande.all.where(personne_id: @personne.id)
+
   end
 
   def edit
