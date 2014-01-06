52f9e35223ed9bc46b92cfde12348122b5c4039d
Alexandre NARBONNE <alexandre.narbonne@gadz.org>
2014-01-06 16:30:31 +0100

    Externalisation de la config du CAS

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -224,29 +224,6 @@ Devise.setup do |config|
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
-  # ==> OmniAuth
-  # Add a new OmniAuth provider. Check the wiki for more information on setting
-  # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
-  config.omniauth :developer unless Rails.env.production?
-
-  require "omniauth-cas"
-
-  config.omniauth :cas, name: "GadzOrg",
-                  host:      'auth.gadz.org:443',
-                  login_url: '/cas/login',
-                  logout_url: '/cas/logout',
-                  service_validate_url: '/cas/serviceValidate',
-                  saml_validate_url: '/cas/samlValidate',
-                  saml_time_url: '/cas',
-                  saml: true,
-                  ssl: true,
-                  disable_ssl_verification: true,
-                  return_url: false,
-                  renew: true
-
-
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
