f2035fb9cb780c827a9db539ad16803fc378b3e7
Alexandre NARBONNE <alexandre.narbonne@gadz.org>
2014-01-06 17:11:25 +0100

    Correction du model User pour le CAS

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:cas]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:GadzOrg]
 
 
   private
