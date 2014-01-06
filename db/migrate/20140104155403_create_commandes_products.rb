e9ffcfbed89aa9ed18e8b3961d01dc2b6e7331ea
Alexandre NARBONNE <alexandre.narbonne@gadz.org>
2014-01-06 12:27:29 +0100

    suppresion de la migration defectueuse (commande_products)

--- db/migrate/20140104155403_create_commandes_products.rb
+++
@@ -1,8 +0,0 @@
-class CreateCommandesProducts < ActiveRecord::Migration
-  def change
-    create_table :commandes_products do |t|
-    	t.belongs_to :commande
-    	t.belongs_to :product
-    end
-  end
-end
