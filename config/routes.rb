5a4925372ab8e0ab2d3b6d0d3a93aaec08f08b1f
Alexandre NARBONNE <alexandre.narbonne@gadz.org>
2014-01-08 12:16:28 +0100

    [INFRA-157] Ajout du controller Users, Modification du model Commandes, Ajout des configurations associés aux assurance, modification du seed en conséquence"

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ LooklaDev::Application.routes.draw do
   resources :personnes
   resources :products
 
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
