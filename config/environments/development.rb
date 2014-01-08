11a6c57267011075d573513581cdaac447748a2e
Blaked <dorian.becker@gadz.org>
2014-01-08 22:49:21 +0100

    visual improvments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,10 @@ LooklaDev::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
