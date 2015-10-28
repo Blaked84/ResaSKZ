33dea19df02ae0aeea4b65856ca721dea1b704e9
Dorian BECKER <dorian.becker@gadz.org>
2015-10-28 22:54:36 +0100

    [INFRA-517] platal fix

--- app/models/plataltransaction.rb
+++ app/models/plataltransaction.rb
@@ -1,4 +1,5 @@
 class Plataltransaction < ActiveRecord::Base
-		establish_connection "platal_#{Rails.env}"
+		#establish_connection "platal_#{Rails.env}"
+		establish_connection "platal"
 		self.table_name = "gpayment_transactions"
 end
