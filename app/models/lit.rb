950c43fe3c99c5c7aa9ed539995a5c7d0e6c0dbd
blaked <dorian.becker@gadz.org>
2016-01-08 14:56:20 +0100

    amélioration gestion des lits

--- app/models/lit.rb
+++ app/models/lit.rb
@@ -10,4 +10,29 @@ class Lit < ActiveRecord::Base
 		self.personnes << personne
 	end
 
+	# import des lits et chambres depuis un csv
+	# atribution des chambres accèssibles au personne à partir des numéros de commande 
+	def import_from_csv (csv_file)
+
+		#lire les header pour creer les lits et chambres
+			# on crée la chambre
+			# on crée le lit
+			# on ajoute le lit à la chambre
+
+		# pour chaque ligne
+			# pour chaque colonne
+				# si la valeur est à 1, on ajoute la personne dans la liste des pessones qui ont acces à la chambre
+	
+	# exemple à modifier:
+  	# CSV.foreach(csv_file.path,headers: true) do |row|
+  	# 	Chambre.create(
+  	# 		event_id: row['event_id'],
+  	# 		tbk_id: row['tbk_id'],
+  	# 		zone: row['zone'],
+  	# 		numero: row['numero'],
+  	# 		nbrplace: row['nbr_place'],
+  	# 		)
+  	end
+  end
+
 end
